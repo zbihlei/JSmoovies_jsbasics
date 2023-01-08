5832993339681fc65287be55c9720d418ee683bb
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-01-08 14:13:34 +0200

    add moovie list right way

--- js/script.js
+++ js/script.js
@@ -14,65 +14,118 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+
+document.addEventListener('DOMContentLoaded', ()=>{
 
 
-const addblocks = document.querySelectorAll('.promo__adv>img'),
-      genre = document.querySelector('.promo__genre'),
-      backgr = document.querySelector('.promo__bg'),
-      films = document.querySelector('.promo__interactive-list'),
-      subm = document.querySelector('button'),
-      inpVal = document.querySelector('.adding__input'),
-      delButton = document.querySelector('.delete');
-
-
-addblocks.forEach(item =>{
-    item.remove();
-});
-
-genre.textContent = 'драма';
-
-backgr.style = 'background: url("img/bg.jpg") center center / cover no-repeat; background-position: top;';
-
-// films.innerHTML = '';
-
-// movieDB.movies.sort();
-
-
-let newList =[];
-function addElementToList(list){
-     newList = list.concat();
-
-    subm.addEventListener('click', (ev)=>{
-        let newMoovie = inpVal.value;
-
-        newList.push(newMoovie); 
+    const movieDB = {
+        movies: [
+            "логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
     
-    films.innerHTML= '';
+    
+    const addblocks = document.querySelectorAll('.promo__adv>img'),
+          genre = document.querySelector('.promo__genre'),
+          backgr = document.querySelector('.promo__bg'),
+          films = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
 
-    newList.forEach((fl, i) =>{  
+          addForm.addEventListener('submit', (event)=>{
+            event.preventDefault();
 
-            films.innerHTML += `
+            const newFilm = addInput.value;
+            const favorite = checkbox.checked;
+
+            if(newFilm){
+
+                movieDB.movies.push(newFilm);
+                sortArray(movieDB.movies);
+    
+                createMoovieList(movieDB.movies, films);
+            }
+
+            event.target.reset();
+
+
+        });
+          
+          
+        const deleteAdv = (arr)=>{
+               
+    arr.forEach(item =>{
+        item.remove();
+    });
+    
+     };
+
+        const makeChanges = ()=>{
+    genre.textContent = 'драма'; 
+    backgr.style = 'background: url("img/bg.jpg") center center / cover no-repeat; background-position: top;';
+       };
+
+
+        const sortArray = (arr)=>{
+            arr.sort();
+        };
+
+    
+    function createMoovieList(films, parent){
+        parent.innerHTML = '';
+        
+       films.forEach((fl, i) =>{  
+        
+            parent.innerHTML += `
             <li class="promo__interactive-item">${i+1} ${fl}
             <div class="delete"></div>
             </li>
             `;
-
-            
-    });
-    ev.preventDefault();
+        });
+        
+    }
+    sortArray(movieDB.movies);
+    makeChanges();
+    deleteAdv(addblocks);
+    createMoovieList(movieDB.movies, films);
     
-    });
     
-};
-
-
-addElementToList(movieDB.movies);
+    //my way to add films
+    
+    // let newList =[];
+    // function addElementToList(list){
+    //      newList = list.concat();
+    
+    //     subm.addEventListener('click', (ev)=>{
+    //         let newMoovie = inpVal.value;
+    
+    //         newList.push(newMoovie); 
+        
+    //     films.innerHTML= '';
+    
+    //     newList.forEach((fl, i) =>{  
+    
+    //             films.innerHTML += `
+    //             <li class="promo__interactive-item">${i+1} ${fl}
+    //             <div class="delete"></div>
+    //             </li>
+    //             `;
+    
+                
+    //     });
+    //     ev.preventDefault();
+        
+    //     });
+        
+    // };
+    
+    
+    // addElementToList(movieDB.movies);
+    
+    
+});
